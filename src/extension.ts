4b0f0658e5b267e89517a6cdc80f55dd382ec828
keatontom <20kt34@queens.ca>
2024-08-12 13:40:23 -0400

    Changes to be committed: 	modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -8,60 +8,6 @@ import * as https from 'https';
 import { validateNamingConventions } from './diagnostics';
 import { validateYangFile } from './diagnostics';
 
-// function validateNamingConventions(document: vscode.TextDocument, diagnostics: vscode.DiagnosticCollection) {
-//     const text = document.getText();
-//     const moduleRegex = /module\s+([\w-]+)\s*{/;
-//     const namespaceRegex = /namespace\s+"([^"]+)";/;
-//     const prefixRegex = /prefix\s+([\w-]+);/;
-
-//     const moduleMatch = moduleRegex.exec(text);
-//     const namespaceMatch = namespaceRegex.exec(text);
-//     const prefixMatch = prefixRegex.exec(text);
-
-//     let diagnosticList: vscode.Diagnostic[] = [];
-
-//     if (moduleMatch) {
-//         const moduleName = moduleMatch[1];
-//         const moduleStart = moduleMatch.index + 7; // 'module ' length is 7
-//         const moduleRange = new vscode.Range(document.positionAt(moduleStart), document.positionAt(moduleStart + moduleName.length));
-//         if (!moduleName.startsWith('telus-')) {
-//             diagnosticList.push(new vscode.Diagnostic(
-//                 moduleRange,
-//                 `Module name "${moduleName}" should start with "telus-"`,
-//                 vscode.DiagnosticSeverity.Warning
-//             ));
-//         }
-//     }
-
-//     if (namespaceMatch) {
-//         const namespace = namespaceMatch[1];
-//         const namespaceStart = namespaceMatch.index + 11; // 'namespace "' length is 11
-//         const namespaceRange = new vscode.Range(document.positionAt(namespaceStart), document.positionAt(namespaceStart + namespace.length));
-//         if (!namespace.startsWith('https://tinaa.telus.com/')) {
-//             diagnosticList.push(new vscode.Diagnostic(
-//                 namespaceRange,
-//                 `Namespace "${namespace}" should start with "https://tinaa.telus.com/"`,
-//                 vscode.DiagnosticSeverity.Warning
-//             ));
-//         }
-//     }
-
-//     if (prefixMatch) {
-//         const prefix = prefixMatch[1];
-//         const prefixStart = prefixMatch.index + 7; // 'prefix ' length is 7
-//         const prefixRange = new vscode.Range(document.positionAt(prefixStart), document.positionAt(prefixStart + prefix.length));
-//         if (!prefix.startsWith('telus-')) {
-//             diagnosticList.push(new vscode.Diagnostic(
-//                 prefixRange,
-//                 `Prefix "${prefix}" should start with "telus-"`,
-//                 vscode.DiagnosticSeverity.Warning
-//             ));
-//         }
-//     }
-
-//     diagnostics.set(document.uri, diagnosticList);
-// }
-
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
